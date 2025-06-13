c99305cb4382de407cf9d5f0d93d0e24408c2502
Detective-yokai <sarciapatrick25@gmail.com>
2025-06-13 11:53:57 +0800

    Initial commit

---
+++ js/auth.js
@@ -0,0 +1,3 @@
+    // Replace with your own Supabase project values
+    const SUPABASE_URL = 'https://naqwdawmumzokqisfcnh.supabase.co';
+    const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5hcXdkYXdtdW16b2txaXNmY25oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk3MzkzMTgsImV4cCI6MjA2NTMxNTMxOH0.1Edyb2N8kQ9hkWq8FSPOdQnpplrcH7S2cUmCIo04jHY';
